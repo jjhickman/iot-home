0a5363b1a2fcd12300423a6cd67ea6f4da57b19d
Josh Hickman <jjhickman@protonmail.com>
2020-09-08 02:11:25 -0400

    finished first phase of panoptic service - generic person detection

--- iot-hub/panoptic-service/panoptic-service.py
+++ iot-hub/panoptic-service/panoptic-service.py
@@ -1,56 +1,155 @@
 import pika
 import socketio
+import collections
+import math
+import asyncio
+from io import BytesIO
+import base64
+import common
+import cv2
+import socketio
+import numpy as np
 import json
 import time
-#### Check for Josh or anomaly for up to X seconds
-#### if anomaly detected:
-##### record anomaly for Y seconds
-#### close stream
+import re
+from PIL import Image
 
-def load_labels(path):
-    print('Loading labels')
-    p = re.compile(r'\s*(\d+)(.+)')
-    with open(path, 'r', encoding='utf-8') as f:
-       lines = (p.match(line).groups() for line in f.readlines())
-       return {int(num): text.strip() for num, text in lines}
+JOBS = {
+    'person_detection': {
+        'model': 'mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
+    }
+}
+THRESHOLD = 0.4
+TOP_K = 10
 
-def load_encodings():
-    print('Loading facial embeddings')
+ConnectTime = time.time()
+Interpreter = None
+Result = {}
+Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
+sio = None
+class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
+    """Bounding box.
+    Represents a rectangle which sides are either vertical or horizontal, parallel
+    to the x or y axis.
+    """
+    __slots__ = ()
 
-def create_interpreter(models_path, type):
-    model = ''
-    print('Loading model')
-    return model
+def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
+    """Returns list of detected objects."""
+    boxes = common.output_tensor(interpreter, 0)
+    class_ids = common.output_tensor(interpreter, 1)
+    scores = common.output_tensor(interpreter, 2)
+    count = int(common.output_tensor(interpreter, 3))
 
-def load_job(job):
-    data = json.load(job)
-    source = ''
-    print('Loading job')
-    return source
+    def make(i):
+        ymin, xmin, ymax, xmax = boxes[i]
+        return Object(
+            id=int(class_ids[i]),
+            score=scores[i],
+            bbox=BBox(xmin=np.maximum(0.0, xmin),
+                      ymin=np.maximum(0.0, ymin),
+                      xmax=np.minimum(1.0, xmax),
+                      ymax=np.minimum(1.0, ymax)))
 
-def process_source(source):
-    result = -1
-    print('Processing stream')
-    return result
+    return [make(i) for i in range(top_k) if scores[i] >= score_threshold]
+
+def load_interpreter(model):
+    interpret = common.make_interpreter(model)
+    interpret.allocate_tensors()
+    return interpret
+
+def load_job(config, job):
+    try:
+        data = json.loads(job)
+        source = data['source']
+        job_type = data['job_type']
+        job_interpreter = load_interpreter(config['model_directory'] + '/' + JOBS[job_type]['model'])
+        print('Loading job of type: {} with model {} using source {}'.format(job_type, JOBS[job_type]['model'], source))
+        return job_type, job_interpreter, source
+    except ValueError:
+        print('Failed parsing job')
+        return '', None, ''
+
+def process_webstream(source):
+    global Result, ConnectTime
+    Result = {}
+    sio = socketio.Client()
+    
+    def job_finished():
+        global Result
+        Result = {
+            'job_type': 'person_detection',
+            'message': 'NONE'
+        }
+        sio.sleep(30)
+        print('Notifying server of job finished: {}'.format(time.time()))
+        sio.emit('finished', 'FINISHED!')
+
+    @sio.event
+    def connect():
+        global ConnectTime
+        ConnectTime = time.time()
+        print('Connected at {}! Processing stream...'.format(ConnectTime))
+    
+    @sio.event
+    def connect_error():
+        print("The connection failed!")
+
+    @sio.on('image')
+    def on_image(message):
+        global Interpreter, Result, ConnectTime
+        if (time.time() < (ConnectTime + 30)):
+            try:
+                image = Image.open(BytesIO(base64.b64decode(message['data'])))
+                common.set_input(Interpreter, image)
+                start = time.time()
+                Interpreter.invoke()
+                end = time.time()
+                print('Interpreted image in {} seconds'.format(end - start))
+                objects = get_output(Interpreter, score_threshold=THRESHOLD, top_k=TOP_K)
+                if len(objects) > 0:
+                    print('Person detected!')
+                    Result = {
+                    'job_type': 'person_detection',
+                    'message': 'FOUND'
+                    }
+                    sio.disconnect()
+            except:
+                print('Error parsing message from socketio server')
+                sio.disconnect()
+        else:
+            print('Disconnecting. Session expired!')
+            sio.disconnect()
+
+    try:
+        print('Connecting to {} at {}'.format(source, time.time()))
+        sio.connect(source)
+        sio.start_background_task(job_finished)
+        sio.wait()
+    except:
+        print('Error connecting to {}'.format(source))
+    print('Job finished. Disconnected from server at {}. Result: {}'.format(source, Result))
+    print(time.time())
+
+    return Result
 
 def run(config, channel):
-    for method_frame, properties, body in channel.consume(config['input_queue']):
-        print(method_frame, properties, body)
-
-        source = load_job(body)
-
-        result = process_source(source)
+    global Interpreter
+    for method_frame, _, body in channel.consume(config['input_interpreter_queue']):
+        print('New RabbitMQ message: {}'.format(body))
+        job_type, Interpreter, source = load_job(config, body)
+        if job_type == 'person_detection':
+            Result = process_webstream(source)
         channel.basic_publish(exchange='',
-                      routing_key=config['output_queue'],
-                      body=result)
-        # load right model based on job type
-
+                      routing_key=config['output_interpreter_queue'],
+                      body=json.dumps(Result))
         channel.basic_ack(method_frame.delivery_tag)
 
 
 if __name__ == '__main__':
     with open('./config.json') as contents:
         config = json.load(contents)
+    print('Config: {}'.format(config))
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=config['rabbitmq_host']))
     channel = connection.channel()
     channel.basic_qos(prefetch_count=1) # tell RabbitMQ not to give more than one message at a time
