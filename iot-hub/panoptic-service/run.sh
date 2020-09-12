9dfb407d8ee03f0fa04829e986cf8b38684ce037
Josh Hickman <jjhickman@protonmail.com>
2020-09-12 19:07:54 +0000

    commented out setting read rules for camera in run.sh (2) -- must be done only once

--- iot-hub/panoptic-service/run.sh
+++ iot-hub/panoptic-service/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo echo 'SUBSYSTEM=="vchiq",MODE="0666"' | sudo tee --append /etc/udev/rules.d/99-camera.rules
+#sudo echo 'SUBSYSTEM=="vchiq",MODE="0666"' | sudo tee --append /etc/udev/rules.d/99-camera.rules # <----- Only run once!
 docker run --network host --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/video0:/dev/video0 --device /dev/vchiq -v /opt/vc:/opt/vc --restart unless-stopped -v /dev/bus/usb:/dev/bus/usb -it panoptic-ubuntu-arm64v8:latest /bin/bash
