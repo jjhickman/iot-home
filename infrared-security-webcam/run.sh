90c73bbc5834177c21de97b9af2078e097726ad2
Josh Hickman <jjhickman@protonmail.com>
2020-09-12 10:29:03 +0100

    updated successful Docker build

--- infrared-security-webcam/run.sh
+++ infrared-security-webcam/run.sh
@@ -1,1 +1,2 @@
-docker run --privileged --network host --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/gpiomem --device /dev/vchiq -v /opt/vc:/opt/vc -it webcamv0:latest /bin/bash
+#!/bin/bash
+docker run --privileged --network host --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/gpiomem --device /dev/vchiq -v /opt/vc:/opt/vc -it raspbian/stretch:latest /bin/bash
