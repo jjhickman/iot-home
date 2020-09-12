d8014dac87b4ae684834463d46c20e2f96ce65fe
Josh Hickman <jjhickman@protonmail.com>
2020-09-12 19:03:19 +0000

    removed priviledged flag in run.sh(2). Updated Dockerfile with python app dependencies and setup

--- infrared-security-webcam/run.sh
+++ infrared-security-webcam/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo echo 'SUBSYSTEM=="vchiq",MODE="0666"' | sudo tee --append /etc/udev/rules.d/99-camera.rules
-docker run --privileged --network host --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/video0:/dev/video0 --device /dev/vchiq -v /opt/vc:/opt/vc --restart unless-stopped -v /dev/bus/usb:/dev/bus/usb -it security-webcam:latest /bin/bash
+docker run --network host --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/video0:/dev/video0 --device /dev/vchiq -v /opt/vc:/opt/vc --restart unless-stopped -v /dev/bus/usb:/dev/bus/usb -it security-webcam:latest /bin/bash
