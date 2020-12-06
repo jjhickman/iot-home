7703e18e30dd12c489d6bbde75fb314291844275
Josh Hickman <jjhickman@protonmail.com>
2020-12-06 09:40:15 -0500

    Added amplify

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+Amplify.configure(config);
 ReactDOM.render(
   <React.StrictMode>
     <App />
