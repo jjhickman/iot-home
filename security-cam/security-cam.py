aa72b5f6f4b10a0275b2a6dc842713ae763ae7fb
Josh Hickman <jjhickman@protonmail.com>
2021-01-24 19:50:30 -0500

    fixed security-cam socket.io dependency again

--- security-cam/security-cam.py
+++ security-cam/security-cam.py
@@ -39,7 +39,7 @@ async def index(request):
         <body>
         <h1>""" + socket.gethostname() + """</h1>
         <img id='image' src=''/>
-        <script src="https://cdn.socket.io/socket.io-2.4.1.min.js"></script>
+        <script src="https://cdn.socket.io/socket.io-3.0.1.min.js"></script>
         <script>
             const socket = io.connect('""" + stream_url + """');
             socket.on('image', (image) => {
