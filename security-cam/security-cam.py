ffd5f81c21b4dcfe515e8ea8dc045ca386c52b0a
Josh Hickman <jjhickman@protonmail.com>
2021-03-01 18:04:39 -0500

    improved handling for movement detection in security-cam

--- security-cam/security-cam.py
+++ security-cam/security-cam.py
@@ -3,7 +3,6 @@ import sys
 import os
 import ssl
 from variables import Variables
-import argparse
 import socket
 import asyncio
 import socketio
@@ -14,6 +13,8 @@ import functools
 import logging
 import pathlib
 import json
+from skimage.measure import compare_ssim
+from async_timeout import timeout
 import aiohttp
 from aiohttp import web
 from middleware import setup_middlewares
@@ -55,26 +56,27 @@ async def index(request):
 def motion_found(threshold, frame):
     global static_back
     motion = False
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0) 
-  
     if static_back is None: 
         static_back = gray 
         return motion
-  
-    diff_frame = cv2.absdiff(static_back, gray) 
-    thresh_frame = cv2.threshold(diff_frame, threshold, 255, cv2.THRESH_BINARY)[1] 
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2) 
-    cnts,_ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-  
+    (score, diff) = compare_ssim(static_back, gray, full=True)
+    diff = (diff * 255).astype("uint8")
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    #diff_frame = cv2.absdiff(static_back, gray)
+    #thresh_frame = cv2.threshold(diff_frame, threshold, 255, cv2.THRESH_BINARY)[1] 
+    #thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2) 
+    #cnts, _ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in cnts: 
-        if cv2.contourArea(contour) < 10000: 
-            continue
-        motion = True
-  
-        (x, y, w, h) = cv2.boundingRect(contour) 
-    if motion:
-        logger.debug("Motion detected!")
+        if cv2.contourArea(contour) >= 5000:
+            logger.debug("BIG AREA OF CHANGE: " + str(cv2.contourArea(contour)))
+            motion = True
+            (x, y, w, h) = cv2.boundingRect(contour)
+    if motion == True:
+        logger.debug("Motion detected")
     return motion
 
 
@@ -91,35 +93,39 @@ async def stream(app):
         while True:
             ret, frame = app['capture'].read()
             if ret == False:
-                logger.info("FAILED READING FROM CAPTURE")
+                logger.error("FAILED READING FROM CAPTURE")
                 break
             ret, jpg_image = cv2.imencode('.jpg', frame)
             base64_image = base64.b64encode(jpg_image)
             await app['socket'].emit('image', base64_image)
 
-            await asyncio.sleep(refresh_seconds)
-            awake = awake_time == -1 or time.time() >= awake_time
+            awake = time.time() >= awake_time
             if awake and motion_found(int(app['config'].threshold), frame):
-                timeout = aiohttp.ClientTimeout(total=5)
-                async with aiohttp.ClientSession(timeout=timeout) as session:
-                    try:
-                        async with session.post(app['config'].hub_url, auth=aiohttp.BasicAuth(app['config'].hub_username, app['config'].hub_password,'utf-8'), timeout=timeout, json={'source': stream_url, 'time': round(time.time() * 1000)}) as response:
-                            status = await response.status()
-                            data = await response.json()
-                            if status != 200:
-                                logger.error("Status: " + str(status) + " - sleeping for default 60 seconds...")
+                try:
+                    async with timeout(5):
+                        async with aiohttp.ClientSession() as session:
+                            try:
+                                async with session.post(app['config'].hub_url, auth=aiohttp.BasicAuth(app['config'].hub_username, app['config'].hub_password,'utf-8'), json={'source': stream_url, 'time': round(time.time() * 1000)}) as response:
+                                    status = await response.status()
+                                    data = await response.json()
+                                    if status != 200:
+                                        logger.error("Status: " + str(status) + " - sleeping for default 60 seconds...")
+                                        awake_time = time.time() + 60
+                                    else:
+                                        if data['sleep_seconds'] and int(data['sleep_seconds']) > 0:
+                                            awake_time = time.time() + int(data['sleep_seconds'])
+                                            logger.info("Sleeping for " + str(data['sleep_seconds']) + " seconds...")
+                                        else:
+                                            awake_time = time.time() +  60
+                                            logger.error("No sleep_seconds field in JSON response. Sleeping for default 60 seconds...")
+                            except aiohttp.ClientError as e:
+                                await asyncio.sleep(refresh_seconds)
+                                logger.error("HTTP Client POST error: " + str(e))
                                 awake_time = time.time() + 60
-                            else:
-                                data = await response.json()
-                                if data['sleep_seconds'] and int(data['sleep_seconds']) > 0:
-                                    awake_time = time.time() + int(data['sleep_seconds'])
-                                    logger.info("Sleeping for " + str(data['sleep_seconds']) + " seconds...")
-                                else:
-                                    awake_time = time.time() +  60
-                                    logger.error("No sleep_seconds field in JSON response. Sleeping for default 60 seconds...")
-                    except aiohttp.ClientError as e:
-                        logger.error("HTTP Client POST error: " + str(e))
-                        awake_time = time.time() + 60
+                except asyncio.TimeoutError:
+                    await asyncio.sleep(refresh_seconds)
+                    logger.debug("Timed out trying to make request to hub")
+            await asyncio.sleep(refresh_seconds)
         logger.debug('Ended stream!')
     except asyncio.CancelledError:
         logger.debug('Stream cancelled')
